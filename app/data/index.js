2318184029139c2be9fafb3232f37ad374e76634
Aneta Kasprzak <anet.kasprzak@gmail.com>
2024-07-23 19:37:25 +0100

    update link

--- app/data/index.js
+++ app/data/index.js
@@ -10,7 +10,7 @@ export const infoComponentsData = [
     id: 1,
     title: "Create and share your photo stories.",
     text: "Photosnap is a platform for photographers and visual storytellers. We make it easy to share photos, tell stories and connect with others.",
-    link: { text: "GET AN INVITE", href: "/form", color: "white" },
+    link: { text: "GET AN INVITE", href: "/invite", color: "white" },
     image: {
       mobile: "./assets/home/mobile/create-and-share.jpg",
       tablet: "./assets/home/tablet/create-and-share.jpg",
